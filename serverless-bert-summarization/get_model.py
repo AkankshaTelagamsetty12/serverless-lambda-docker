9fce4c53b28ef11150fbbc015cacdda8f06e61ca
AkankshaTelagamsetty12 <telagamsetty.a@northeastern.edu>
2022-04-14 09:50:08 -0400

    summ

---
+++ serverless-bert-summarization/get_model.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:501022651f2df4af1607e25491864281228a53133647e36413e3bebb0b57e3ff
+size 587
---
+++ serverless-bert-summarization/handler.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9417d27f2ee04379e1639d985f0b5521fb009541512ec355a73a0b77976acbc8
+size 1543
